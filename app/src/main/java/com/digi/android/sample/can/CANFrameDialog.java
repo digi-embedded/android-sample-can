b71cfce5d8cdcd7b7e24116b4204a96455349907
Tatiana Leon <tatiana.leon@digi.com>
2016-01-20 20:38:36 +0100

    digi_sdk_addon: Changes to work with the CAN API in the addon.
    Signed-off-by: Tatiana Leon <tatiana.leon@digi.com>

--- app/src/main/java/com/digi/android/sample/can/CANFrameDialog.java
+++ app/src/main/java/com/digi/android/sample/can/CANFrameDialog.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014-2015 Digi International Inc.,
+ * Copyright (c) 2014-2016 Digi International Inc.,
  * All rights not expressly granted are reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
@@ -10,7 +10,7 @@
  * =======================================================================
  */
 
-package com.digi.android.CAN;
+package com.digi.android.sample.can;
 
 import java.util.regex.Pattern;
 
@@ -34,30 +34,30 @@ public class CANFrameDialog extends Dialog implements TextWatcher {
 	private final static String HEX_PATTERN = "[0-9a-fA-F]{0,2}";
 
 	// Variables.
-	private String frame;
+	private String data;
 	
 	private Button okButton;
 
 	private EditText[] texts;
 	
-	private CANSample sample;
+	private CANSampleActivity sampleActivity;
 	
-	private int canID;
+	private int interfaceNumber;
 	
 	/**
 	 * Class constructor. Instances a new object of type CAN Frame Dialog
 	 * with the given parameters.
 	 * 
-	 * @param sample CAN Sample activity.
-	 * @param frame the frame to represent.
-	 * @param canID the identifier of the CAN interface
+	 * @param sampleActivity CAN Sample activity.
+	 * @param data the frame to represent.
+	 * @param interfaceNumber the CAN interface number.
 	 */
-	public CANFrameDialog(CANSample sample, String frame, int canID) {
-		super(sample);
-		setTitle(sample.getResources().getString(R.string.frame_edit));
-		this.sample = sample;
-		this.frame = frame;
-		this.canID = canID;
+	public CANFrameDialog(CANSampleActivity sampleActivity, String data, int interfaceNumber) {
+		super(sampleActivity);
+		setTitle(sampleActivity.getResources().getString(R.string.frame_edit));
+		this.sampleActivity = sampleActivity;
+		this.data = data;
+		this.interfaceNumber = interfaceNumber;
 	}
 
 	@Override
@@ -83,29 +83,24 @@ public class CANFrameDialog extends Dialog implements TextWatcher {
 	 * Initializes dialog UI components and sets listeners.
 	 */
 	private void initializeUIComponents() {
-		EditText byte0 = (EditText) findViewById(R.id.byte0);
-		EditText byte1 = (EditText) findViewById(R.id.byte1);
-		EditText byte2 = (EditText) findViewById(R.id.byte2);
-		EditText byte3 = (EditText) findViewById(R.id.byte3);
-		EditText byte4 = (EditText) findViewById(R.id.byte4);
-		EditText byte5 = (EditText) findViewById(R.id.byte5);
-		EditText byte6 = (EditText) findViewById(R.id.byte6);
-		EditText byte7 = (EditText) findViewById(R.id.byte7);
-		byte0.addTextChangedListener(this);
-		byte1.addTextChangedListener(this);
-		byte2.addTextChangedListener(this);
-		byte3.addTextChangedListener(this);
-		byte4.addTextChangedListener(this);
-		byte5.addTextChangedListener(this);
-		byte6.addTextChangedListener(this);
-		byte7.addTextChangedListener(this);
-		texts = new EditText[]{byte0, byte1, byte2, byte3, byte4, byte5, byte6, byte7};
+		texts = new EditText[]{
+				(EditText) findViewById(R.id.byte0),
+				(EditText) findViewById(R.id.byte1),
+				(EditText) findViewById(R.id.byte2),
+				(EditText) findViewById(R.id.byte3),
+				(EditText) findViewById(R.id.byte4),
+				(EditText) findViewById(R.id.byte5),
+				(EditText) findViewById(R.id.byte6),
+				(EditText) findViewById(R.id.byte7)};
+
+		for (EditText t: texts)
+			t.addTextChangedListener(this);
 		
 		okButton = (Button)findViewById(R.id.ok_button);
 		okButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				sample.updateCANFrame(frame, canID);
+				sampleActivity.updateCANFrame(data, interfaceNumber);
 				dismiss();
 			}
 		});
@@ -123,8 +118,8 @@ public class CANFrameDialog extends Dialog implements TextWatcher {
 	 * Fills all the dialog byte fields with the given frame.
 	 */
 	private void fillFields() {
-		byte[] bytes = HexUtils.hexStringToByteArray(frame.replace(" ", ""));
-		for (int i = 0; i < 8; i ++)
+		byte[] bytes = HexUtils.hexStringToByteArray(data.replace(" ", ""));
+		for (int i = 0; i < texts.length; i ++)
 			texts[i].setText(HexUtils.byteArrayToHexString(new byte[]{bytes[i]}));
 	}
 	
@@ -133,9 +128,9 @@ public class CANFrameDialog extends Dialog implements TextWatcher {
 	 */
 	private void updateFrame() {
 		// First validate.
-		for (int i = 0; i < 8; i ++) {
+		for (int i = 0; i < texts.length; i ++) {
 			if (!Pattern.matches(HEX_PATTERN, texts[i].getText())) {
-				Toast.makeText(sample, "Frame has invalid characters", Toast.LENGTH_SHORT).show();
+				Toast.makeText(sampleActivity, "Frame has invalid characters", Toast.LENGTH_SHORT).show();
 				okButton.setEnabled(false);
 				return;
 			}
@@ -143,7 +138,7 @@ public class CANFrameDialog extends Dialog implements TextWatcher {
 
 		// Build the frame.
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < 8; i++) {
+		for (int i = 0; i < texts.length; i++) {
 			String fieldText = texts[i].getText().toString();
 			int diff = 2 - fieldText.length();
 			for (int j = 0; j < diff; j++)
@@ -153,7 +148,7 @@ public class CANFrameDialog extends Dialog implements TextWatcher {
 				sb.append(" ");
 		}
 
-		frame = sb.toString().toUpperCase();
+		data = sb.toString().toUpperCase();
 		okButton.setEnabled(true);
 	}
 }
